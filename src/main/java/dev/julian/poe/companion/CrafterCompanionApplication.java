ca934ef722dc9e86f9802f3fd2447cb66cb9a095
Julian Degutis <juliang@kyros.com.br>
2024-05-15 23:31:53 -0300

    Initial

---
+++ src/main/java/dev/julian/poe/companion/CrafterCompanionApplication.java
@@ -0,0 +1,13 @@
+package dev.julian.poe.companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrafterCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrafterCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/julian/poe/companion/CrafterCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.julian.poe.companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrafterCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
